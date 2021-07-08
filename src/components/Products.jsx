c50ed1bf71db41195aab384a5c9ed01960746698
Aldo Camacho <aldo.acamacho98@gmail.com>
2021-07-08 15:25:23 -0700

    Created an AppContext and used it in Header and Products

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import Product from './Product';
-
+import AppContext from '../context/AppContext';
 import '../styles/components/Products.scss';
 
-const Products = ({ products }) => {
-  console.log(products);
+const Products = () => {
+  const { state, addToCart } = useContext(AppContext);
+  const { products } = state;
+
+  const handleAddToCart = (product) => () => {
+    addToCart(product);
+  };
+  console.log(state.cart);
+
   return (
     <div className="Products">
       <div className="Products__items">
         {products.map((product) => (
-          <Product key={product.id} product={product} />
+          <Product
+            key={product.id}
+            product={product}
+            handleAddToCart={handleAddToCart}
+          />
         ))}
       </div>
     </div>
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import Products from '../components/Products';
-import initialState from '../initialState';
 
 const Home = () => {
   return (
     <>
-      <Products products={initialState.products} />
+      <Products />
     </>
   );
 };
