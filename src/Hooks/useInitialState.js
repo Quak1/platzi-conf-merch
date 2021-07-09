57ed0fcf1aec73ce576f75ca8f143348c737b57d
Aldo Camacho <aldo.acamacho98@gmail.com>
2021-07-09 13:12:59 -0700

    Changed state.cart to manage multiple count of product in cart

--- src/Hooks/useInitialState.js
+++ src/Hooks/useInitialState.js
@@ -4,19 +4,44 @@ import initialState from '../initialState';
 
 const useInitialState = () => {
   const [state, setState] = useState(initialState);
+  const { cart } = state;
 
   const addToCart = (payload) => {
-    setState({
-      ...state,
-      cart: [...state.cart, payload],
+    const stateId = cart.findIndex((item) => {
+      return item.id === payload.id;
     });
+
+    if (stateId === -1) {
+      setState({
+        ...state,
+        cart: [...state.cart, { ...payload, count: 1 }],
+      });
+    } else {
+      const items = [...cart];
+
+      const item = { ...cart[stateId] };
+      item.count += 1;
+      items[stateId] = item;
+
+      setState({ ...state, cart: items });
+    }
   };
 
   const removeFromCart = (payload) => {
-    setState({
-      ...state,
-      cart: state.cart.filter((items) => items.id !== payload.id),
-    });
+    if (cart[payload.id].count > 1) {
+      const items = [...cart];
+
+      const item = { ...cart[stateId] };
+      item.count -= 1;
+      items[stateId] = item;
+
+      setState({ ...state, cart: items });
+    } else {
+      setState({
+        ...state,
+        cart: state.cart.filter((items) => items.id !== payload.id),
+      });
+    }
   };
 
   return {
